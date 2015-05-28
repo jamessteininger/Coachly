7226f4cb997c1de251704f38af43188d305aa12a
James Steinigner <jesteininger@gmail.com>
2015-05-28 08:00:11 +0000

    Fix sender and receiver workflows

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :feedbacks
   has_many :feedback_requests
+  
+  def sent_feedback_requests(current_user)
+    return FeedbackRequest.where(sender_id: current_user.id)
+  end
+  
+  def sent_feedbacks(current_user)
+    return Feedback.where(author_id: current_user.id)
+  end
 end
